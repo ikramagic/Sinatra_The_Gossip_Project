5d25ac7b6225a6fbfac7b3210bf5e2c21ace980b
ikramiste <ikrame.saadi@gmail.com>
2023-10-23 13:00:08 +0200

    added redirection & responsiveness

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,10 +1,17 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base 
     #en héritant de Sinatra, la classe AppCon obtient les fonctionnalités de Sinatra
     get '/' do
     erb :index
     end
 
-    get '/gossips/new/' do
+    get '/gossips/new' do
     erb :new_gossip
+    end
+
+    post '/gossips/new' do
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+        redirect '/'
     end
 end
