c6b0600313efb37d8a15747592e3f153c12ffdd8
ikramiste <ikrame.saadi@gmail.com>
2023-10-23 16:30:57 +0200

    added get to display gossips 1 by 1

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,9 @@
-require 'gossip'
+require_relative 'gossip'
 
 class ApplicationController < Sinatra::Base 
     #en héritant de Sinatra, la classe AppCon obtient les fonctionnalités de Sinatra
     get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all} #une méthode locals, qui permet d'envoyer à notre fichier ERB des variables que l'on utilisera
     end
 
     get '/gossips/new' do
@@ -13,5 +13,15 @@ class ApplicationController < Sinatra::Base
     post '/gossips/new' do
         Gossip.new(params["gossip_author"], params["gossip_content"]).save
         redirect '/'
+    end
+
+    get '/gossips/all' do
+        erb :all_gossips, locals: {gossips: Gossip.all}
+    end
+
+    get '/gossips/:id' do
+    index = params[:id].to_i #on indique que la variable index sera égale au chiffre qui sera trouvé dans l'URL
+    @element = Gossip.all[index] 
+        erb :show
     end
 end
