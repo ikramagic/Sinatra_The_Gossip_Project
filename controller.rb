f235d4f4e2da657a95d0fb2b680200375ee2a58b
ikramiste <ikrame.saadi@gmail.com>
2023-10-23 10:38:53 +0200

    moved

--- controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < Sinatra::Base 
-    #en héritant de Sinatra, la classe AppCon obtient les fonctionnalités de Sinatra
-  get '/' do
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
-  end
-
-  #run! if app_file == $0
-    #permet d’exécuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier
-end
