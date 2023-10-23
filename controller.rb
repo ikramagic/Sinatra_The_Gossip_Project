7d40685b2882f4ee12a66a55d6e1352c53c08e8f
ikramiste <ikrame.saadi@gmail.com>
2023-10-23 10:09:38 +0200

    edited to comply with run from config.ru

--- controller.rb
+++ controller.rb
@@ -1,10 +1,9 @@
-require 'bundler'
-Bundler.require
-
-class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base 
+    #en héritant de Sinatra, la classe AppCon obtient les fonctionnalités de Sinatra
   get '/' do
     "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
   end
 
-  run! if app_file == $0
+  #run! if app_file == $0
+    #permet d’exécuter le code de la classe sans avoir à écrire un ApplicationController.new.perform en bas du fichier
 end
